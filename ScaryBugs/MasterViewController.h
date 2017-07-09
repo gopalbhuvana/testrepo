961dc1e66748a11600703cf260b1a3593ad41847
bhuvanagopal <bhuvanagopal@users.noreply.github.com>
2017-07-09 18:32:11 -0500

    ugh one more test comment

--- ScaryBugs/MasterViewController.h
+++ ScaryBugs/MasterViewController.h
@@ -5,7 +5,7 @@
 //  Created by Bhuvana on 7/9/17.
 //  Copyright © 2017 Bhuvana. All rights reserved.
 //
-
+//ok trying this out with a new comment
 #import <UIKit/UIKit.h>
 
 @class DetailViewController;
