ae8285b477727540cf0dc670bcf479713595f31e
bhuvanagopal <bhuvanagopal@users.noreply.github.com>
2017-07-09 19:22:18 -0500

    well thats all for today

--- ScaryBugs/MasterViewController.h
+++ ScaryBugs/MasterViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @class DetailViewController;
-
+//ok one more test commit!
 @interface MasterViewController : UITableViewController
 
 @property (strong, nonatomic) DetailViewController *detailViewController;
